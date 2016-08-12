76d29841995de0e0408a84ee601ce9ab2d662cfe
Daniel Evans <dan@rekt.io>
2016-08-12 20:10:12 +0100

    Fix entering & exiting vehicle animations
    - Don't crash if the seat doesn't have a door
    - Make RHS animations count as entering or exiting animations

--- rwengine/src/ai/CharacterController.cpp
+++ rwengine/src/ai/CharacterController.cpp
@@ -219,12 +219,14 @@ bool Activities::EnterVehicle::update(CharacterObject *character, CharacterContr
 	
 	auto entryDoor = vehicle->getSeatEntryDoor(seat);
 	auto entryPos = vehicle->getSeatEntryPositionWorld(seat);
+	auto entryPosLocal = vehicle->getSeatEntryPosition(seat);
+	auto isRhs = entryPosLocal.x > 0.f;
 
 	auto anm_open = character->animations.car_open_lhs;
 	auto anm_enter = character->animations.car_getin_lhs;
 	auto anm_pullout = character->animations.car_pullout_lhs;
 
-	if( entryDoor->dummy->getDefaultTranslation().x > 0.f )
+	if (isRhs)
 	{
 		anm_open = character->animations.car_open_rhs;
 		anm_enter = character->animations.car_getin_rhs;
@@ -318,6 +320,8 @@ bool Activities::ExitVehicle::update(CharacterObject *character, CharacterContro
 {
 	RW_UNUSED(controller);
 
+	auto vehicle = character->getCurrentVehicle();
+
 	if (jacked) {
 		auto anm_jacked_lhs = character->animations.car_jacked_lhs;
 		auto anm_jacked_rhs = character->animations.car_jacked_lhs;
@@ -329,20 +333,20 @@ bool Activities::ExitVehicle::update(CharacterObject *character, CharacterContro
 			}
 		}
 		else {
-			if (character->getCurrentVehicle() == nullptr) return true;
+			if (vehicle == nullptr) return true;
 
-			auto vehicle = character->getCurrentVehicle();
 			auto seat = character->getCurrentSeat();
-			auto door = vehicle->getSeatEntryDoor(seat);
+			auto exitPos = vehicle->getSeatEntryPositionWorld(seat);
+			auto exitPosLocal = vehicle->getSeatEntryPosition(seat);
+			auto isRhs = exitPosLocal.x > 0.f;
 
 			character->rotation = vehicle->getRotation();
 
 			// Exit the vehicle immediatley
-			auto exitpos = vehicle->getSeatEntryPosition(seat);
 			character->enterVehicle(nullptr, seat);
-			character->setPosition(exitpos);
+			character->setPosition(exitPos);
 
-			if (door->dummy->getDefaultTranslation().x > 0.f) {
+			if (isRhs) {
 				character->playActivityAnimation(anm_jacked_rhs, false, true);
 			}
 			else {
@@ -353,15 +357,17 @@ bool Activities::ExitVehicle::update(CharacterObject *character, CharacterContro
 		return false;
 	}
 
-	if( character->getCurrentVehicle() == nullptr ) return true;
+	if( vehicle == nullptr ) return true;
 
-	auto vehicle = character->getCurrentVehicle();
 	auto seat = character->getCurrentSeat();
 	auto door = vehicle->getSeatEntryDoor(seat);
+	auto exitPos = vehicle->getSeatEntryPositionWorld(seat);
+	auto exitPosLocal = vehicle->getSeatEntryPosition(seat);
+	auto isRhs = exitPosLocal.x > 0.f;
 
 	auto anm_exit = character->animations.car_getout_lhs;
 
-	if( door->dummy->getDefaultTranslation().x > 0.f )
+	if (isRhs)
 	{
 		anm_exit = character->animations.car_getout_rhs;
 	}
@@ -387,10 +393,9 @@ bool Activities::ExitVehicle::update(CharacterObject *character, CharacterContro
 
 	if( character->animator->getAnimation(AnimIndexAction) == anm_exit ) {
 		if( character->animator->isCompleted(AnimIndexAction) ) {
-			auto exitpos = vehicle->getSeatEntryPositionWorld(seat);
 
 			character->enterVehicle(nullptr, seat);
-			character->setPosition(exitpos);
+			character->setPosition(exitPos);
 
 			if (isDriver)
 			{
